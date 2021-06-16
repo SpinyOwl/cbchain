2b8da35a6fea2585ba86d2dfab356a91467b5a20
ShchAlexander <shchalexander@gmail.com>
2021-06-16 04:18:38 +0200

    Updated module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,7 @@ open module com.spinyowl.cbchain {
     requires org.lwjgl.natives;
     requires org.lwjgl.glfw;
     requires org.lwjgl.glfw.natives;
+
+    exports com.spinyowl.cbchain;
+    exports com.spinyowl.cbchain.impl;
 }
