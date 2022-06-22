11e5bbcbc6feb13eff0d948a03a0ae5f4008926e
SpinyOwl <shchalexander@gmail.com>
2022-06-22 12:27:35 +0200

    Downgrade Java version to 1.8

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-open module com.spinyowl.cbchain {
-    requires org.lwjgl;
-    requires org.lwjgl.natives;
-    requires org.lwjgl.glfw;
-    requires org.lwjgl.glfw.natives;
-
-    exports com.spinyowl.cbchain;
-    exports com.spinyowl.cbchain.impl;
-}
