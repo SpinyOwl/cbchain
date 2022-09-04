42394245c327bbb93721d6641f412b86d93181a3
SpinyOwl <shchalexander@gmail.com>
2022-09-04 23:10:26 +0200

    Revert "Downgrade Java version to 1.8"
    This reverts commit 11e5bbcbc6feb13eff0d948a03a0ae5f4008926e.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+open module com.spinyowl.cbchain {
+    requires org.lwjgl;
+    requires org.lwjgl.natives;
+    requires org.lwjgl.glfw;
+    requires org.lwjgl.glfw.natives;
+
+    exports com.spinyowl.cbchain;
+    exports com.spinyowl.cbchain.impl;
+}
